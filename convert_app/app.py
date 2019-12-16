6b46cee04778f92adac4784953355af7262c077a
rasoulpoordelan <rasoulpoordelan@gmail.com>
2019-12-17 02:31:57 +0330

    review

--- convert_app/app.py
+++ convert_app/app.py
@@ -12,8 +12,6 @@ file_name='data.csv'
 if len( sys.argv) > 1 :
      file_name = sys.argv[1]
 
-print(len(sys.argv))
-
 db_name='data.db' 
 if len(sys.argv) > 2 :
     db_name= sys.argv[2]
@@ -39,11 +37,13 @@ with open(file_name, mode='r') as csv_file:
     line_count = 0
     for row in csv_reader:
         data = Data(id = row["id"],
-                    timestamp = datetime.datetime.now(),
+                    timestamp = datetime.datetime.strptime(row["timestamp"], '%Y-%m-%d %H:%M:%S.%f'),
                     temperature = Decimal(row["temperature"]) ,
                     duration= ut.convert_string_to_timedelta(row["duration"]))
         session.add(data)
         session.commit()
+
+print("import finish")
 
 
 
